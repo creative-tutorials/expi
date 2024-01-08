b3778e7942a973deb3a5ae17eaaa2d17a18a9d9f
creative-tutorials <treasurealekhojie@yahoo.com>
2024-01-08 18:31:34 +0100

    refactor: Modify production API URL
    Signed-off-by: creative-tutorials <treasurealekhojie@yahoo.com>

--- frontend/hooks/api-url.ts
+++ frontend/hooks/api-url.ts
@@ -1,4 +1,4 @@
 export const getAPIURL = () => {
   if (process.env.NODE_ENV === "development") return "http://localhost:8080";
-  else return "https://api-wishme.vercel.app";
+  else return "https://api-expi.vercel.app";
 };
